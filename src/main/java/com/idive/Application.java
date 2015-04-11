2a2466a0f2804babd0cfd764fd6a004fdb9a8ad3
Sylvain <lbsylvain1@aol.com>
2015-04-12 08:02:57 +1100

    configuration log4j2 ajout banner

--- src/main/java/com/idive/Application.java
+++ src/main/java/com/idive/Application.java
@@ -1,5 +1,7 @@
 package com.idive;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,7 +11,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
+    static final Logger logger = LogManager.getLogger(Application.class);
+
     public static void main(String[] args) throws Exception {
+        logger.info(">>>>>>>>>>>>LAAAA");
         SpringApplication.run(Application.class, args);
     }
 }
