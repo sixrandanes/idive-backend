3b216cd9e173ad766676fecc2908e5eb9d37a927
Sylvain <lbsylvain1@aol.com>
2015-04-11 21:17:06 +1100

    1/ ajout starter web 2/ config root classe

--- src/main/java/com/idive/Application.java
+++ src/main/java/com/idive/Application.java
@@ -1,8 +1,15 @@
 package com.idive;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Created by Sylvain on 11/04/2015.
  */
+@SpringBootApplication
 public class Application {
 
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
 }
